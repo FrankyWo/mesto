a2f466f298b655932eb07b860757c0d9a8154368
Kubenko Ilya <FrankyWoo10@yandex.ru>
2022-12-03 18:41:12 +0800

    Complite/beforRewiew

--- script/index.js
+++ script/index.js
@@ -1,6 +1,6 @@
 const popup = document.querySelector('.popup');
 const buttonEditOpen = document.querySelector('.profile__button-edit');
-const buttonClose = document.querySelector('.popum__button-close');
+const buttonClose = document.querySelector('.popup__button-close');
 const getName = document.querySelector('.profile__name');
 const getJob = document.querySelector('.profile__description');
 let editForm = document.querySelector('.popup__container');
@@ -11,18 +11,18 @@ function openPopup() {
     popup.classList.add('popup__opened');
     nameInput.value = getName.textContent;
     jobInput.value = getJob.textContent;
-}
+};
 
 function closePopup() {
     popup.classList.remove('popup__opened');
-}
+};
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
     getName.textContent = `${nameInput.value}`;
     getJob.textContent = `${jobInput.value}`;
     closePopup();
-}
+};
 
 buttonEditOpen.addEventListener('click', openPopup);
 buttonClose.addEventListener('click', closePopup);
